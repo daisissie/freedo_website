3400f4401fd0700e52d7246e5d290f03f6c8412e
Yuxi Dai <dai.yuxi1112@gmail.com>
2026-03-02 23:23:20 -0500

    change name

--- config.example.js
+++ config.example.js
@@ -1,3 +1,3 @@
 // Copy this file to config.js and fill in your key
 // config.js is gitignored and will never be committed
-window.FAL_KEY = 'you_fal_key_here';
+window.FAL_KEY = 'your_fal_key_here';
